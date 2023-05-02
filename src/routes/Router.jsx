69eac0f5e29b965dbc35c5517219d0e67d91ef00
swapnilaanam <swapnilaanam211@gmail.com>
2023-05-02 16:55:55 +0600

    added react-router with routes and layout

---
+++ src/routes/Router.jsx
@@ -0,0 +1,33 @@
+import { createBrowserRouter } from "react-router-dom";
+import Main from "../layout/Main";
+import Home from "../pages/Home/Home/Home";
+import Blog from "../pages/Blog/Blog/Blog";
+import Login from "../pages/Login/Login/Login";
+import Register from "../pages/Login/Register/Register";
+
+const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <Main></Main>,
+        children: [
+            {
+                path: '/',
+                element: <Home></Home>
+            },
+            {
+                path: '/blog',
+                element: <Blog></Blog>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>,
+            },
+            {
+                path: '/register',
+                element: <Register></Register>
+            }
+        ]
+    }
+]);
+
+export default router;
