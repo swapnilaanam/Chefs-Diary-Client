25b1dac2e6b6f90c443fb17ad7611c1ebc80908b
swapnilaanam <swapnilaanam211@gmail.com>
2023-05-02 21:57:24 +0600

    added registration page

--- src/pages/Login/Login/Login.jsx
+++ src/pages/Login/Login/Login.jsx
@@ -4,19 +4,20 @@ import { Link } from 'react-router-dom';
 
 const Login = () => {
     return (
-        <div className="card w-full max-w-xl bg-gray-200 shadow-lg mx-auto my-16 rounded-lg">
+        <div className="card w-full max-w-xl bg-gray-200 shadow-lg mx-auto my-14 rounded-lg">
             <form className="card-body mx-2">
+                <h4 className="form-title text-center text-2xl text-black font-medium">Please Login</h4>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text text-xl">Email</span>
                     </label>
-                    <input type="email" id="l-name" name="email" placeholder="Enter Your Email" className="input input-bordered w-full" required/>
+                    <input type="email" id="l-email" name="email" placeholder="Enter Your Email" className="input input-bordered w-full" required />
                 </div>
                 <div className="form-control">
                     <label className="label">
                         <span className="label-text text-xl">Password</span>
                     </label>
-                    <input type="password" id="l-password" name="password" placeholder="Enter Your Password" className="input input-bordered w-full" required/>
+                    <input type="password" id="l-password" name="password" placeholder="Enter Your Password" className="input input-bordered w-full" required />
                 </div>
                 <div className="form-control mt-6">
                     <button className="btn text-white text-xl capitalize">Login</button>
@@ -25,16 +26,16 @@ const Login = () => {
             <div className="divider mx-7">OR</div>
             <div className='flex justify-center gap-8 py-7'>
                 <button className="btn text-lg capitalize flex items-center gap-3">
-                    <FaGoogle className="text-xl"/> 
+                    <FaGoogle className="text-xl" />
                     <span>Sign In With Google</span>
                 </button>
                 <button className="btn text-lg capitalize flex items-center gap-3">
-                    <FaGithub className="text-xl"/>
+                    <FaGithub className="text-xl" />
                     Sign In With Github
                 </button>
             </div>
-            <p className='text-center text-black py-6 text-xl font-medium'>
-                Doesn't Have An Account? 
+            <p className='text-center text-black pb-6 text-xl font-medium'>
+                Doesn't Have An Account?
                 <Link to="/register" className="ms-2 text-green-600 text-2xl font-bold"> Register.</Link>
             </p>
         </div>
--- src/pages/Login/Register/Register.jsx
+++ src/pages/Login/Register/Register.jsx
@@ -1,9 +1,47 @@
 import React from 'react';
+import { FaGoogle, FaGithub } from "react-icons/fa";
+import { Link } from 'react-router-dom';
 
 const Register = () => {
     return (
         <div>
-            <h2>This is register</h2>
+            <div className="card w-full max-w-xl bg-gray-200 shadow-lg mx-auto mt-8 mb-14 rounded-lg">
+                <form className="card-body mx-2">
+                    <h4 className="form-title text-center text-2xl text-black font-medium">Please Register</h4>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text text-xl">Name</span>
+                        </label>
+                        <input type="text" id="r-name" name="name" placeholder="Enter Your Name" className="input input-bordered w-full" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text text-xl">Photo URL</span>
+                        </label>
+                        <input type="photo" id="r-photo" name="photo" placeholder="Enter Your Photo URL" className="input input-bordered w-full" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text text-xl">Email</span>
+                        </label>
+                        <input type="email" id="r-email" name="email" placeholder="Enter Your Email" className="input input-bordered w-full" required />
+                    </div>
+                    <div className="form-control">
+                        <label className="label">
+                            <span className="label-text text-xl">Password</span>
+                        </label>
+                        <input type="password" id="r-password" name="password" placeholder="Enter Your Password" className="input input-bordered w-full" required />
+                    </div>
+                    <div className="form-control mt-6">
+                        <button className="btn text-white text-xl capitalize">Register</button>
+                    </div>
+                </form>
+                
+                <p className='text-center text-black pb-6 text-xl font-medium'>
+                    Already Have An Account?
+                    <Link to="/login" className="ms-2 text-green-600 text-2xl font-bold"> Login.</Link>
+                </p>
+            </div>
         </div>
     );
 };
