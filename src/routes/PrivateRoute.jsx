c2bb2c1e66b6f51059736242569f50cba9cec18e
swapnilaanam <swapnilaanam211@gmail.com>
2023-05-04 23:29:45 +0600

    showed a spinner when the data is in a loading state

--- src/routes/PrivateRoute.jsx
+++ src/routes/PrivateRoute.jsx
@@ -6,7 +6,6 @@ const PrivateRoute = ({ children }) => {
     const { user, loading } = useContext(AuthContext);
 
     const location = useLocation();
-    console.log(location);
 
     if (loading) {
         return (
