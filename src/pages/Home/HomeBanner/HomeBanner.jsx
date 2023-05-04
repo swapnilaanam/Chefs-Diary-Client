96baf9ab2af3236add726869e47e3c790a3a147c
swapnilaanam <swapnilaanam211@gmail.com>
2023-05-04 18:51:32 +0600

    added active route to indicate the route which we are visting

--- src/pages/Home/HomeBanner/HomeBanner.jsx
+++ src/pages/Home/HomeBanner/HomeBanner.jsx
@@ -14,10 +14,10 @@ const HomeBanner = () => {
             <div className="hero-content text-center text-neutral-content">
                 <div className="max-w-lg">
                     <h2 className="mb-5 text-5xl bg-black bg-opacity-10 font-bold capitalize p-4 rounded">
-                        We are <span className="text-green-400">Chef's Diary</span>
+                        We are <span className="text-green-500">Chef's Diary</span>
                     </h2>
                     <p className="mb-6 text-2xl font-medium bg-black bg-opacity-10 capitalize p-1 rounded mx-6">
-                        Discover authentic Indian food flavors with our chef's recipes!
+                        Discover authentic Indian food flavors with our chefs recipes!
                     </p>
                     <button onClick={handleScroll} className="border-0 bg-green-600 text-2xl text-white font-bold capitalize px-5 py-3 rounded-lg flex items-center gap-3 mx-auto">
                         <span>See Our Indian Chefs</span>
